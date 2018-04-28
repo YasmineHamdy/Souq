25d7fe30e3c58a425264407c4422c6ffc2dd6061
nadakhaled <nada@nada.zte.com.cn>
2018-04-28 19:46:42 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProductComponent } from './product/product.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SigninComponent } from './signin/signin.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    ProductComponent,
+    ProfileComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
